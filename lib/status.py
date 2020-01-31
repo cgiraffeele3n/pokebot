655a02ffc23c484b101f2822ced6d407483ba110
cgiraffeele3n <51842744+cgiraffeele3n@users.noreply.github.com>
2020-02-01 00:32:54 +0900

    Update status.py

--- lib/status.py
+++ lib/status.py
@@ -3,12 +3,23 @@ import math
 
 class Status:
 
-
-    def muhosei(self, syuzokuchi):
-        return math.floor(((syuzokuchi*2 + 31 + 252/4) * 50/100 + 5) * 1.0)
-
-    def hosei(self, syuzokuchi):
-        return math.floor(((syuzokuchi*2 + 31 + 252/4) * 50/100 + 5) * 1.1)
+    """
+    無振りのステータスを返す
+    """
 
     def mufuri(self, syuzokuchi):
-        return math.floor(((syuzokuchi*2 + 31) * 50/100 + 5) * 1.0)
+        return str(math.floor(((syuzokuchi*2 + 31) * 50/100 + 5) * 1.0))
+
+    """
+    無補正のステータス実数値を返す
+    """
+
+    def muhosei(self, syuzokuchi):
+        return str(math.floor(((syuzokuchi*2 + 31 + 252/4) * 50/100 + 5) * 1.0))
+
+    """
+    補正のステータス実数値を返す
+    """
+
+    def hosei(self, syuzokuchi):
+        return str(math.floor(((syuzokuchi*2 + 31 + 252/4) * 50/100 + 5) * 1.1))
