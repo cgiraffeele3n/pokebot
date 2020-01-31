3a84224e4b422937744bd9f1922e7b8da37e89d7
cgiraffeele3n <51842744+cgiraffeele3n@users.noreply.github.com>
2020-02-01 00:22:39 +0900

    Update discordPost.py

--- discordPost.py
+++ discordPost.py
@@ -2,6 +2,7 @@ import discord
 import random
 import os
 from lib import csvwrapper
+from lib import status
 
 # トークン
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
@@ -11,8 +12,13 @@ client = discord.Client()
 
 getCsv = './csv/poke.csv'
 
+# CsvWrapperインスタンス
 csv = csvwrapper.CsvWrapper()
 
+# statusインスタンス
+status = status.Status()
+
+# ステータスcsvファイルの読み込み
 readcsv = csv.readCsv(getCsv)
 
 # 起動時に動作する処理
@@ -39,14 +45,25 @@ async def on_message(message):
     if message.content == "/pass":
         await message.channel.send(str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)))
 
+    # $[ポケモン名]と一致する種族値を返す(部分一致)
     if message.content.startswith("$?", 0):
         for row in readcsv:
             if message.content.lstrip("$?") in row[0]:
                 await message.channel.send(row[0] + ":" + row[1] + "-" + row[2] + "-" + row[3] + "-" + row[4] + "-" + row[5] + "-" + row[6])
 
-    for row in readcsv:
-        if message.content == "$" + row[0]:
-            await message.channel.send(row[0] + ":" + row[1] + "-" + row[2] + "-" + row[3] + "-" + row[4] + "-" + row[5] + "-" + row[6])
+    # $[ポケモン名]と一致する種族値を返す(完全一致)
+    if message.content.startswith("$", 0):
+        for row in readcsv:
+            if message.content == "$" + row[0]:
+                await message.channel.send(row[0] + ":" + row[1] + "-" + row[2] + "-" + row[3] + "-" + row[4] + "-" + row[5] + "-" + row[6])
 
+    # $[ポケモン名]と一致する素早さステータスを返す
+    if message.content.startswith("$S", 0):
+        for row in readcsv:
+            if message.content == "$S" + row[0]:
+                await message.channel.send(row[0] + "の素早さは")
+                await message.channel.send("無振り:" + status.mufuri(int(row[6])))
+                await message.channel.send("無補正:" + status.muhosei(int(row[6])))
+                await message.channel.send("補正:" + status.hosei(int(row[6])))
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
